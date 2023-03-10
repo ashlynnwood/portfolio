5c18b4af1acdb8f06523db6c151e0f36b5b1fe18
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-03-09 21:33:16 -0600

    Added sun/moon icons

--- script.js
+++ script.js
@@ -33,6 +33,6 @@ if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
 
   if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
+    toggle.checked = true;
   }
 }
